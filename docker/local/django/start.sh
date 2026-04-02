d3094d37f9ad17f8ae9091cfd6f8136d47eccf18
chimdi247 <chimdi247@gmail.com>
2026-04-02 05:54:20 +0300

    11 commit

--- docker/local/django/start.sh
+++ docker/local/django/start.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
 set -o errexit
+
 set -o pipefail
+
 set -o nounset
 
-echo "PostgreSQL is available"
-
-# Ensure static directory exists (no chown!)
-mkdir -p /app/staticfiles
-
-# Run migrations
 python manage.py migrate --no-input
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Start server
 exec python manage.py runserver 0.0.0.0:8000
