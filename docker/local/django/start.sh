ed6e228d18f34d45ce2d008fd7b30fc35d333c69
chimdi247 <chimdi247@gmail.com>
2026-04-02 05:04:40 +0300

    9th commit

--- docker/local/django/start.sh
+++ docker/local/django/start.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
 set -o errexit
-
 set -o pipefail
-
 set -o nounset
 
+# Ensure the staticfiles folder exists and is writable
+STATIC_DIR="/app/staticfiles"
+mkdir -p "${STATIC_DIR}"
+chown -R django:django "${STATIC_DIR}"
+chmod -R 775 "${STATIC_DIR}"
+
+# Run migrations
 python manage.py migrate --no-input
+
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Start the Django development server
 exec python manage.py runserver 0.0.0.0:8000
